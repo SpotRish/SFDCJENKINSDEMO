72fd399cd868005216f80b5ad58f7452f4962f13
SpotRish <rishabhrathore257@gmail.com>
2020-05-07 18:04:20 +0530

    Initial Commit

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,16 @@
+declare module "@salesforce/resourceUrl/SiteSamples" {
+    var SiteSamples: string;
+    export default SiteSamples;
+}
+declare module "@salesforce/resourceUrl/bike_assets" {
+    var bike_assets: string;
+    export default bike_assets;
+}
+declare module "@salesforce/resourceUrl/bikefiltericon" {
+    var bikefiltericon: string;
+    export default bikefiltericon;
+}
+declare module "@salesforce/resourceUrl/lccEbikes" {
+    var lccEbikes: string;
+    export default lccEbikes;
+}
